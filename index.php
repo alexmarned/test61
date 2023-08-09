2c64d62f05db14fc5cddf8373e46bd59dc64d76f
alexmarned <alexmarned@ukr.net>
2023-08-09 08:38:51 +0300

    first change

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         <?php
         // put your code here
+        echo 'hello world';
         ?>
     </body>
 </html>
