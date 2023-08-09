aac7ce6e0a8474d3a1392c6a46ad8e9eedfff571
alexmarned <alexmarned@ukr.net>
2023-08-09 09:24:56 +0300

    четвёртое изменение

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         // put your code here
         echo 'hello world';
         echo 'hello 2';
-        echo 'hello 3';
+        echo 'hello 4 из ветки 2';
+           echo 'hello 5 из ветки 3';
         ?>
     </body>
 </html>
