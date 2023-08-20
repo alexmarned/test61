a865c06907d7e0567f9ffcb78e2afa98a8a55e6c
alexmarned <alexmarned@ukr.net>
2023-08-20 16:21:50 +0300

    добввил класс

---
+++ temp.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Http;
+
+/**
+ * Description of temp
+ *
+ * @author alexm
+ */
+class temp {
+    //put your code here
+    private function functionName($a, $b) {
+        return $a + $b;
+    }
+}
